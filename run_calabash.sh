90e1af7c83142fe490d38efbb5ebea82900e39b9
daolao <dalao1002@gmail.com>
2019-04-24 00:01:49 +0200

    try to fix

--- run_calabash.sh
+++ run_calabash.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 cp ./app/build/outputs/apk/debug/app-debug.apk ./calabahTests/app.apk
-cd ./calabahTests/
-cat output
+# cd ./calabahTests/
+cat ./output
 # calabash-android run app.apk
