05c78a6290985d1010b0dc55c3eb70aca26901b3
daolao <dalao1002@gmail.com>
2019-04-23 21:42:17 +0200

    try to fix

--- run_calabash.sh
+++ run_calabash.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-find . -name '*'
+find /home/travis/.rvm/rubies/ruby-2.4.1/ -name '*'
 cp ./app/build/outputs/apk/debug/app-debug.apk ./calabahTests/app.apk
 cd ./calabahTests/
 calabash-android run app.apk
